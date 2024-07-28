c84afa69cb7339c0f93f5fe43d7b367120ab2abf
NewNobbie <florezs043@gmail.com>
2024-07-28 10:47:38 -0500

    Chore

---
+++ Restaurant-Management-denifity/src/main/java/com/riwi/Restaurant_Management_denifity/utils/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.riwi.Restaurant_Management_denifity.utils.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
